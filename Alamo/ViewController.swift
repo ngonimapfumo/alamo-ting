337c5e0ee3bf534d79a7d0884b216a88bc12b639
Ngoni <ngonidzashe.mapfumo@ecocashholdings.co.zw>
2024-11-12 22:21:40 +0200

    Initial Commit

---
+++ Alamo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alamo
+//
+//  Created by Ngoni Mapfumo on 2024/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
